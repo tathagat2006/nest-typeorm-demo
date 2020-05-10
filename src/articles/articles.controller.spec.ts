023f7f42a06b353929c93208d0e4e62c98f2d21a
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-05-10 18:56:44 +0530

    added basic structure for articles
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesController } from './articles.controller';
+import { ArticlesService } from './articles.service'
 
 describe('Articles Controller', () => {
   let controller: ArticlesController;
@@ -7,6 +8,7 @@ describe('Articles Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticlesController],
+      providers: [ArticlesService]
     }).compile();
 
     controller = module.get<ArticlesController>(ArticlesController);
--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ArticlesService } from './articles.service'
 
 @Controller('articles')
-export class ArticlesController {}
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService){}
+
+  @Get()
+  async getAllArticles () {
+    return await this.articlesService.getAllArticles()
+  }
+}
